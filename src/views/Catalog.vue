533f9e5f3cf5afce3814719396e7f7e8c731c150
plusgood-steven <plusgoodwu@gmail.com>
2021-03-13 14:29:14 +0800

    update

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,11 +1,11 @@
 <template>
   <el-row>
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <el-button>按鈕</el-button>
+    <el-button type="primary">按鈕</el-button>
+    <el-button type="success">按鈕</el-button>
+    <el-button type="info">按鈕</el-button>
+    <el-button type="warning">按鈕</el-button>
+    <el-button type="danger">按鈕</el-button>
   </el-row>
   <div style="overflow-y: auto;">
     <el-row :gutter="20" style="min-width:1045px">
